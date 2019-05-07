e30f9e5680a648b948c77d4e2a4567e344b75b0c
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2019-05-07 14:44:22 +0200

    add function to generate standardized peaks

--- W2/semi_lagrange.py
+++ W2/semi_lagrange.py
@@ -7,40 +7,46 @@ import useful_functions as uf
 from scipy import interpolate
 
 
+def create_phi(Nx=50, Ny=50, xlim=[-10, 10], ylim=[-10, 10]):
+    """
+    Creates a standardized scalar field for use in the advection equation
+    Takes in 4 arguments: x/y limits and number of points in each direction.
+    """
+    x = np.linspace(xlim[0], xlim[1], Nx)
+    y = np.linspace(ylim[0], ylim[1], Ny)
+    xx, yy = np.meshgrid(x, y)
+
+    # parameters for the peaks
+    sigma_xs = [0.5, 1, 2, 0.8]
+    sigma_ys = [0.5, 1, 0.5, 1]
+    As = [-0.2, 0.7, -0.5, 1]
+    mu_xs = [0, 3, 0, -3]
+    mu_ys = [3, 0, -3, 0]
+
+    # Create the field through addition
+    phi = np.zeros_like(xx)
+    for sigma_x, sigma_y, A, mu_y, mu_x in zip(sigma_xs, sigma_ys,
+                                               As, mu_ys, mu_xs):
+        phi += A * np.exp(-(xx-mu_x)**2/(2*sigma_x**2) -
+                          (yy-mu_y)**2/(2*sigma_y**2))
+
+    return xx, yy, phi
+
 xmin, xmax = -10, 10
 ymin, ymax = -10, 10
 Nx = 50
 Ny = 50
 dt = 1
 
-num_steps = 2*np.pi / dt
+num_steps = 2*np.pi / dt 
 
-sigma_xs = [0.5, 1, 2, 0.8]
-sigma_ys = [0.5, 1, 0.5, 1]
-As = [-0.2, 0.7, -0.5, 1]
-mu_xs = [0, 3, 0, -3]
-mu_ys = [3, 0, -3, 0]
+xx, yy, phi = create_phi()
 
-x = np.linspace(xmin, xmax, Nx)
-y = np.linspace(ymin, ymax, Ny)
-xx, yy = np.meshgrid(x, y)
-
-
-zz = np.zeros_like(xx)
-for sigma_x, sigma_y, A, mu_y, mu_x in zip(sigma_xs, sigma_ys,
-                                           As, mu_ys, mu_xs):
-    zz += A * np.exp(-(xx-mu_x)**2/(2*sigma_x**2) - (yy-mu_y)**2/(2*sigma_y**2)) 
-
-
-# zz = np.sinc(xx - mu_x) * np.sinc(yy - mu_y)
-
-
-zmin = np.amin(zz)
-zmax = np.amax(zz)
+zmin = np.amin(phi)
+zmax = np.amax(phi)
 
 fig, axes = plt.subplots(subplot_kw=dict(projection='3d'))
-# axes = axes.flatten()
-axes.plot_surface(xx, yy, zz, cmap='jet')
+axes.plot_surface(xx, yy, phi, cmap='jet')
 axes.set_title('0')
 axes.view_init(90, 0)
 # for i in range(1,6):
