f61b66f6749c1274695dc565b7f0e82832b8f49a
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2019-05-08 11:11:53 +0200

    add function to calculate k on domain

--- W2/mean_curvature_flow.py
+++ W2/mean_curvature_flow.py
@@ -30,6 +30,21 @@ def read_gray_image(name):
     return im
 
 
+def calc_k_ij_domain(phi, i, j, deltax, deltay, clamp_g=False, use_eps=True):
+    dx = (phi[i+1, j] - phi[i-1, j]/2)*deltax
+    dy = (phi[i, j+1] - phi[i, j-1]/2)*deltay
+    dxx = (phi[i+1, j] - 2*phi[i,j] + phi[i-1, j])/(deltax**2)
+    dyy = (phi[i, j+1] - 2*phi[i, j] + phi[i, j-1])/(deltay**2)
+    dxy = (phi[i+1, j+1] - phi[i+1, j-1] - phi[i-1, j+1] + phi[i-1, j-1])/(4*deltax*deltay)
+    g = np.sqrt(dx**2 + dy**2)
+    if clamp_g:
+        g[g<0.5] = 1
+    kij = (dx*dx * dyy + dy*dy*dxx - 2*dxy*dx*dy) / \
+        (g**3 + use_eps*np.finfo(float).eps)
+    return kij
+
+
+
 if __name__ == "__main__":
     im = read_gray_image('example.bmp')
     phi = bwdist(im)
