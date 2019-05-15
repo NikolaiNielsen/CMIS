e42b434137bd1147015002336a6cd5e281d34b73
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2019-05-15 15:01:49 +0200

    add function to clamp several times in a row. Start Delaunay

--- W3/spring_method.py
+++ W3/spring_method.py
@@ -1,8 +1,8 @@
 
 #%%
-from scipy import interpolate
+from scipy import interpolate, ndimage
 import imageio
-from scipy import ndimage
+from scipy.spatial import Delaunay
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
@@ -10,10 +10,6 @@ from mpl_toolkits.mplot3d import Axes3D
 sys.path.append('../')
 import quality_measures as qa
 import useful_functions as uf
-
-
-bwdist = ndimage.morphology.distance_transform_edt
-np.set_printoptions(threshold=np.inf)
 
 
 def import_data(name='example.bmp', Nverts=500):
@@ -55,59 +51,44 @@ def push_points_inside(X, Y, Gx, Gy, sdf):
 
     return X, Y
 
+
+def push_fully_inside(X, Y, Gx, Gy, sdf, max_tries=3):
+    mask = True
+    counter_max = max_tries + 1
+    for _ in range(counter_max):
+        if np.sum(mask):
+            X, Y = push_points_inside(X, Y, Gx, Gy, sdf)
+
+            points = np.array((Gx.flatten(), Gy.flatten())).T
+            values = sdf.flatten()
+            interp_points = np.array((X, Y)).T
+
+            d = interpolate.griddata(points, values, interp_points)
+            d_nan = np.isnan(d)
+            d[d_nan] = interpolate.griddata(points, values,
+                                            interp_points[d_nan, :],
+                                            method='nearest')
+            mask = d > 0
+        else:
+            return X, Y
+    return X, Y
+
+
+
+
+
 #%% project particles
 Gx, Gy, sdf, X, Y, im = import_data()
 
-X, Y = push_points_inside(X, Y, Gx, Gy, sdf)
-X, Y = push_points_inside(X, Y, Gx, Gy, sdf)
+X, Y = push_fully_inside(X, Y, Gx, Gy, sdf)
+
+points = np.array((X, Y)).T
+T = Delaunay(points)
 
 
-# fig2, ax2 = plt.subplots(subplot_kw=dict(projection='3d'))
-# ax2.plot_surface(Gx, Gy, dx, alpha=0.5)
-# ax2.scatter(X, Y, nx, c='r')
-# limits = np.array((0, 255))
-# extra = np.array((-50, 50))
-# ax2.set_xlim(limits+extra)
-# ax2.set_ylim(limits+extra)
-# ax2.set_title('dx interpolated')
-
-# fig3, ax3 = plt.subplots(subplot_kw=dict(projection='3d'))
-# ax3.plot_surface(Gx, Gy, dy, alpha=0.5)
-# ax3.scatter(X, Y, ny, c='r')
-# limits = np.array((0, 255))
-# extra = np.array((-50, 50))
-# ax3.set_xlim(limits+extra)
-# ax3.set_ylim(limits+extra)
-# ax3.set_title('dy interpolated')
-
-# fig, ax = plt.subplots()
-# ax.imshow(sdf, cmap='Greys_r')
-# ax.quiver(X, Y, nx, ny, units='width')
-# ax.scatter(X,Y, c='r')
-
-# fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
-# ax.plot_surface(Gx, Gy, sdf, alpha=0.5)
-# ax.scatter(X, Y, d, c='r')
-# limits = np.array((0, 255))
-# extra = np.array((-50, 50))
-# ax.set_xlim(limits+extra)
-# ax.set_ylim(limits+extra)
-# ax.set_title('sdf interpolated')
-
-
-
-# fig4, ax4 = plt.subplots()
-# nxf = nx.flatten()
-# nyf = ny.flatten()
-# ax4.plot(np.sort(nxf))
-# ax4.plot(np.sort(nyf))
-
-fig5, ax5 = plt.subplots()
-ax5.imshow(im, cmap='Greys_r')
-ax5.scatter(X,Y)
+fig, ax = plt.subplots()
+ax.imshow(im, cmap='Greys_r')
+ax.triplot(X, Y, T.simplices)
+ax.scatter(X,Y)
 
 plt.show()
-#%% 3D diagnostics
-
-
-#%%
