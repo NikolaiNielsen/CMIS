c889d67821269d20a6578d0d8002cbac22eb5f7a
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2019-05-15 14:41:51 +0200

    add border on random number generation. Flip gradient. Fix typo

--- W3/spring_method.py
+++ W3/spring_method.py
@@ -18,10 +18,10 @@ np.set_printoptions(threshold=np.inf)
 
 def import_data(name='example.bmp', Nverts=500):
     sdf, im = uf.grey_to_sdf(name, ghosts=False)
-    Nx, Ny = sdf.shape
+    Ny, Nx = sdf.shape
     border = 0.5
-    X = np.random.uniform(border, Nx-border, Nverts)
-    Y = np.random.uniform(border, Ny-border, Nverts)
+    X = np.random.uniform(border, Nx-1-border, Nverts)
+    Y = np.random.uniform(border, Ny-1-border, Nverts)
     Gx, Gy = np.meshgrid(np.arange(Nx), np.arange(Ny))
     return Gx, Gy, sdf, X, Y, im
 
@@ -37,7 +37,7 @@ d_nan = np.isnan(d)
 d[d_nan] = interpolate.griddata(points, values, interp_points[d_nan,:],
                                 method='nearest')
 
-dx, dy = np.gradient(sdf)
+dy, dx = np.gradient(sdf)
 
 nx = interpolate.griddata(points, dx.flatten(), interp_points)
 nx_nan = np.isnan(nx)
@@ -49,54 +49,64 @@ ny_nan = np.isnan(ny)
 ny[ny_nan] = interpolate.griddata(points, values, interp_points[ny_nan, :],
                                   method='nearest')
 
-fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
-ax.plot_surface(Gx, Gy, sdf, alpha=0.5)
-ax.scatter(X, Y, d, c='r')
-limits = np.array((0, 255))
-extra = np.array((-50, 50))
-ax.set_xlim(limits+extra)
-ax.set_ylim(limits+extra)
-ax.set_title('sdf interpolated')
 
-fig2, ax2 = plt.subplots(subplot_kw=dict(projection='3d'))
-ax2.plot_surface(Gx, Gy, dx, alpha=0.5)
-ax2.scatter(X, Y, nx, c='r')
-limits = np.array((0, 255))
-extra = np.array((-50, 50))
-ax2.set_xlim(limits+extra)
-ax2.set_ylim(limits+extra)
-ax2.set_title('dx interpolated')
+# fig2, ax2 = plt.subplots(subplot_kw=dict(projection='3d'))
+# ax2.plot_surface(Gx, Gy, dx, alpha=0.5)
+# ax2.scatter(X, Y, nx, c='r')
+# limits = np.array((0, 255))
+# extra = np.array((-50, 50))
+# ax2.set_xlim(limits+extra)
+# ax2.set_ylim(limits+extra)
+# ax2.set_title('dx interpolated')
 
-fig3, ax3 = plt.subplots(subplot_kw=dict(projection='3d'))
-ax3.plot_surface(Gx, Gy, dy, alpha=0.5)
-ax3.scatter(X, Y, nx, c='r')
-limits = np.array((0, 255))
-extra = np.array((-50, 50))
-ax3.set_xlim(limits+extra)
-ax3.set_ylim(limits+extra)
-ax3.set_title('dy interpolated')
-
-fig4, ax4 = plt.subplots()
-nxf = nx.flatten()
-nyf = ny.flatten()
-ax4.plot(np.sort(nxf))
-ax4.plot(np.sort(nyf))
+# fig3, ax3 = plt.subplots(subplot_kw=dict(projection='3d'))
+# ax3.plot_surface(Gx, Gy, dy, alpha=0.5)
+# ax3.scatter(X, Y, ny, c='r')
+# limits = np.array((0, 255))
+# extra = np.array((-50, 50))
+# ax3.set_xlim(limits+extra)
+# ax3.set_ylim(limits+extra)
+# ax3.set_title('dy interpolated')
 
 
-dx = d*nx
-dy = d*ny
+nx = d*nx
+ny = d*ny
+
+
+# fig, ax = plt.subplots()
+# ax.imshow(sdf, cmap='Greys_r')
+# ax.quiver(X, Y, nx, ny, units='width')
+# ax.scatter(X,Y, c='r')
+
+# fig, ax = plt.subplots(subplot_kw=dict(projection='3d'))
+# ax.plot_surface(Gx, Gy, sdf, alpha=0.5)
+# ax.scatter(X, Y, d, c='r')
+# limits = np.array((0, 255))
+# extra = np.array((-50, 50))
+# ax.set_xlim(limits+extra)
+# ax.set_ylim(limits+extra)
+# ax.set_title('sdf interpolated')
+
+
+
+# fig4, ax4 = plt.subplots()
+# nxf = nx.flatten()
+# nyf = ny.flatten()
+# ax4.plot(np.sort(nxf))
+# ax4.plot(np.sort(nyf))
+
+
+
 mask = d > 0
 
-
-X[mask] = X[mask] - dx[mask]
-Y[mask] = Y[mask] - dy[mask]
+X[mask] = X[mask] - nx[mask]
+Y[mask] = Y[mask] - ny[mask]
 
 fig5, ax5 = plt.subplots()
-ax5.imshow(im)
+ax5.imshow(im, cmap='Greys_r')
 ax5.scatter(X,Y)
 
 plt.show()
-
 #%% 3D diagnostics
 
 
