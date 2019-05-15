c47a420703cc5e66389d1a66df6403db68418748
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2019-05-15 17:44:51 +0200

    first attempt at generating points within triangle

--- W3/spring_method.py
+++ W3/spring_method.py
@@ -74,21 +74,65 @@ def push_fully_inside(X, Y, Gx, Gy, sdf, max_tries=3):
     return X, Y
 
 
+def gen_line_from_points(x, p1, p2, min_=True):
+    dx = p2[0] - p1[0]
+    dy = p2[1] - p1[1]
+    if dx == 0:
+        if min_:
+            return min(p1[1], p2[1])
+        else:
+            return max(p1[1], p2[1])
+    else:
+        return dy/dx*(x-p1[0]) + p1[1]
 
 
+def gen_points_in_triangle(v, N=10):
+    p1, p2, p3 = v[np.argsort(v[:, 1])]
+    
+    xmax = np.amax(v[:, 0])
+    xmin = np.amin(v[:, 0])
+
+    x = np.random.uniform(xmin, xmax, N)
+    print(x)
+
+    f1y_max = gen_line_from_points(x, p1, p2, min_=False)
+    f1y_min = gen_line_from_points(x, p1, p2)
+    f2y_max = gen_line_from_points(x, p1, p3, min_=False)
+    f2y_min = gen_line_from_points(x, p1, p3)
+    f3y_max = gen_line_from_points(x, p2, p3, min_=False)
+    f3y_min = gen_line_from_points(x, p2, p3)
+    f_min = np.minimum(f1y_min, f2y_min, f3y_min)
+    f_max = np.maximum(f1y_max, f2y_max, f3y_max)
+
+
+    y = np.random.uniform(f_min, f_max, N)
+    return x, y
 
 #%% project particles
-Gx, Gy, sdf, X, Y, im = import_data()
+# Gx, Gy, sdf, X, Y, im = import_data()
 
-X, Y = push_fully_inside(X, Y, Gx, Gy, sdf)
+# X, Y = push_fully_inside(X, Y, Gx, Gy, sdf)
 
-points = np.array((X, Y)).T
-T = Delaunay(points)
+# points = np.array((X, Y)).T
+# T = Delaunay(points)
 
+
+# fig, ax = plt.subplots()
+# ax.imshow(im, cmap='Greys_r')
+# ax.triplot(X, Y, T.simplices)
+# ax.scatter(X,Y)
+
+# plt.show()
+
+
+#%%
+a = np.array(((1,2),(2,4),(1,3)))
+x, y = gen_points_in_triangle(a)
 
 fig, ax = plt.subplots()
-ax.imshow(im, cmap='Greys_r')
-ax.triplot(X, Y, T.simplices)
-ax.scatter(X,Y)
-
+ax.plot(a[[0,1],0], a[[0,1],1], 'b')
+ax.plot(a[[0,2],0], a[[0,2],1], 'b')
+ax.plot(a[[1,2],0], a[[1,2],1], 'b')
+ax.scatter(a[:,0], a[:,1], s=36)
+ax.scatter(x,y, s=10)
 plt.show()
