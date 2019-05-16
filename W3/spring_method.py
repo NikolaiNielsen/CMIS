c4222a7178763b6ca32436e5e48d61be523933f2
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2019-05-16 15:25:22 +0200

    use bivariate splines all over the place

--- W3/spring_method.py
+++ W3/spring_method.py
@@ -22,10 +22,13 @@ def import_data(name='example.bmp', Nverts=500):
     X = np.random.uniform(border, Nx-1-border, Nverts)
     Y = np.random.uniform(border, Ny-1-border, Nverts)
     Gx, Gy = np.meshgrid(np.arange(Nx), np.arange(Ny))
-    return Gx, Gy, sdf, X, Y, im
+    x = Gx[0, :]
+    y = Gy[:, 0]
+    sdf_spline = interpolate.RectBivariateSpline(y, x, sdf)
+    return Gx, Gy, sdf, X, Y, im, sdf_spline
 
 
-def push_with_splines(x, y, sdf_spline):
+def push_points_inside(x, y, sdf_spline):
     d =  sdf_spline.ev(y, x)
     nx = sdf_spline.ev(y, x, dy=1)
     ny = sdf_spline.ev(y, x, dx=1)
@@ -36,72 +39,33 @@ def push_with_splines(x, y, sdf_spline):
     y[mask] = y[mask] - ny[mask]
     return x,y
 
-def push_fully_splines(x, y, sdf_spline, max_tries=3):
+def push_fully_inside(x, y, sdf_spline, max_tries=3):
     d = sdf_spline.ev(y, x)
 
     mask = d > 0
     counter_max = max_tries + 1
     for _ in range(counter_max):
         if np.sum(mask):
-            x, y = push_with_splines(x, y, sdf_spline)
+            x, y = push_points_inside(x, y, sdf_spline)
             d = sdf_spline.ev(y, x)
-
             mask = d > 0
         else:
             return x, y
     return x, y
 
 
-def push_points_inside(interp_points, Gx, Gy, sdf):
-    # interp_points = np.array((X,Y)).T. Shape = (N,2)
-    points = np.array((Gx.flatten(), Gy.flatten())).T
-    values = sdf.flatten()
-
-    d = interpolate.griddata(points, values, interp_points)
-    dy, dx = np.gradient(sdf)
-    nx = interpolate.griddata(points, dx.flatten(), interp_points)
-    ny = interpolate.griddata(points, dy.flatten(), interp_points)
-    nx = d*nx
-    ny = d*ny
-    mask = d > 0
-    diff = np.array((nx, ny)).T
-    interp_points[mask,:] = interp_points[mask] - diff[mask, :]
-
-    return interp_points
-
-
-def push_fully_inside(X, Y, Gx, Gy, sdf, max_tries=3):
-
-    points = np.array((Gx.flatten(), Gy.flatten())).T
-    values = sdf.flatten()
-    interp_points = np.array((X, Y)).T
-    d = interpolate.griddata(points, values, interp_points)
-    d_nan = np.isnan(d)
-    d[d_nan] = interpolate.griddata(points, values,
-                                    interp_points[d_nan, :],
-                                    method='nearest')
-    mask = d > 0
-    counter_max = max_tries+1
-
-    for _ in range(counter_max):
-        if np.sum(mask):
-            interp_points[mask,:] = push_points_inside(interp_points[mask,:],
-                                                       Gx, Gy, sdf)
-
-            d = interpolate.griddata(points, values, interp_points)
-            d_nan = np.isnan(d)
-            d[d_nan] = interpolate.griddata(points, values,
-                                            interp_points[d_nan, :],
-                                            method='nearest')
-
-            mask = d > 0
-        else:
-            return interp_points
-    return interp_points
-
 
 def gen_points_in_triangle(v, N=10):
+    """
+    Generate N points uniformly distributed inside a triangle, defined by
+    vertices v.
 
+    v is assumed to have the shape (3, 2)
+
+    Formula from
+    http://www.cs.princeton.edu/~funk/tog02.pdf
+    section 4.2, page 8.
+    """ 
     r1 = np.sqrt(np.random.uniform(size=N))
     r2 = np.random.uniform(size=N)
     a = 1-r1
@@ -109,16 +73,27 @@ def gen_points_in_triangle(v, N=10):
     c = r1*r2
     r = np.array((a,b,c))
     points = v.T @ r
-
     return points
 
 
 def verts_from_simplex(simplex, x, y):
+    """
+    Get the coordinates of the vertices in a given simplex.
+    inputs:
+    - simplex - list, len 3 - Integers referencing the vertices
+    - x, y - vector of positions for vertices
+    outputs:
+    - verts: (3,2) array of positions for the vertices in the simplex
+    """
     verts = np.array((x[simplex], y[simplex])).T
     return verts
 
 
 def all_triangles(simplices, x, y):
+    """
+    Generates a (n, 3, 2) array of positions for all vertices in the n
+    simplices
+    """
     N_simplices = simplices.shape[0]
     verts = np.zeros((N_simplices, 3, 2))
     for i, simplex in enumerate(simplices):
@@ -126,49 +101,42 @@ def all_triangles(simplices, x, y):
     return verts
 
 
-def get_outside_triangles(verts, Gx, Gy, sdf, bar=True):
+def discard_outside_triangles(simplices, x, y, sdf_spline):
     """
     runs over the list of triangles, and figures out whether they are outside
     or inside the object
     """
-    outside = []
-    points = np.array((Gx.flatten(), Gy.flatten())).T
-    values = sdf.flatten()
-    if bar:
-        progress = Bar('Ousides', max=verts.shape[0])
+    verts = all_triangles(simplices, x, y)
+    dims = verts.shape
+    N_points = 10
+    triangle_points = np.zeros((dims[0], N_points, 2))
     for i, vert in enumerate(verts):
-        # First we generate 10 points for each triangle
-        interp_points = gen_points_in_triangle(vert)
-        # Then we interpolate sdf on these points
-        d = interpolate.griddata(points, values, interp_points.T)
-        # if points are inside object, then d < 0
-        mask = d < 0
-        num_inside = np.sum(mask)
-        if num_inside < 10:
-            outside.append(i)
-        if bar:
-            progress.next()
+        # First we generate 10 points for each triangle and throw them in the
+        # array
+        triangle_points[i, :, :] = gen_points_in_triangle(vert, N_points).T
+
+    # reshape array for easy d-calculation with splines
+    triangle_points_reshaped = triangle_points.reshape((dims[0] * N_points, 2))
+    # Calculate d for all points
+    d = sdf_spline.ev(triangle_points_reshaped[:, 0],
+                      triangle_points_reshaped[:, 1])
     
-    if bar:
-        progress.finish()
-    return outside
+    d_reshaped = d.reshape((dims[0], 10))
+    d_mask = d_reshaped > 0
+    d_sum = np.sum(d_mask, axis=1)
+    triangles_to_keep = d_sum == 10
+    return simplices[triangles_to_keep]
         
 
 #%% project particles
-Gx, Gy, sdf, X, Y, im = import_data()
-x = Gx[0,:]
-y = Gy[:,0]
-sdf_spline = interpolate.RectBivariateSpline(y, x, sdf)
-push1 = part(push_fully_inside, X, Y, Gx, Gy, sdf)
-push2 = part(push_fully_splines, X, Y, sdf_spline)
-print(timeit.timeit(push1, number=2))
-print(timeit.timeit(push2, number=2))
+Gx, Gy, sdf, X, Y, im, sdf_spline = import_data()
 
-# points = np.array((X,Y)).T
+
+
+points = np.array((X,Y)).T
 # points = push_points_inside(points, Gx, Gy, sdf)
-# T = Delaunay(points)
-# triangles = all_triangles(T.simplices, X, Y)
-# outside = get_outside_triangles(triangles, Gx, Gy, sdf)
+T = Delaunay(points)
+inside_simplices = discard_outside_triangles(T.simplices, X, Y, sdf_spline)
 # print(outside)
 
 # fig, ax = plt.subplots()
