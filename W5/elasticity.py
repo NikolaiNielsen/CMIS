9b2004696177321e0bb9c6dbc3ffc4943a4c7575
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2019-05-31 21:12:29 +0200

    add experiment to analyse quadratic relationship between traction and area increase

--- W5/elasticity.py
+++ W5/elasticity.py
@@ -207,7 +207,7 @@ def ex_analyze_resolution(savefile='areas.npy', savefile2='positions.npy'):
     res_y = positions[:,1] - 0
     [x1, y1, x1_new, y1_new, simplices1] = np.load('raw_exps1.npy')
     [x2, y2, x2_new, y2_new, simplices2] = np.load('raw_exps2.npy')
-    fig, ax = plt.subplots(ncols=2, nrows=2, figsize=(7, 4.5))
+    fig, ax = plt.subplots(ncols=2, nrows=2, figsize=(5.5, 3.5))
     ax1, ax2, ax3, ax4 = ax.flatten()
     ax1.plot(max_areas, res_areas)
     ax1.set_title('Growth in area')
@@ -225,14 +225,14 @@ def ex_analyze_resolution(savefile='areas.npy', savefile2='positions.npy'):
 
     ax3.triplot(x1, y1, simplices1)
     ax3.triplot(x1_new, y1_new, simplices1)
-    ax3.set_title(r'Deformation: Max $A^e$: 2 m$^2$')
+    ax3.set_title(r'Max $A^e$: 2 m$^2$')
     ax3.set_xlabel('x, [m]')
     ax3.set_ylabel('y, [m]')
     ax3.set_aspect('equal')
 
     ax4.triplot(x2, y2, simplices2, linewidth=0.5)
     ax4.triplot(x2_new, y2_new, simplices2, linewidth=0.5)
-    ax4.set_title('Deformation: Max $A^e$: 0.001 m$^2$')
+    ax4.set_title('Max $A^e$: 0.001 m$^2$')
     ax4.set_xlabel('x, [m]')
     ax4.set_ylabel('y, [m]')
     ax4.set_aspect('equal')
@@ -243,9 +243,9 @@ def ex_analyze_resolution(savefile='areas.npy', savefile2='positions.npy'):
 
 def ex_with_external2():
     contour = [np.array([[0, 0], [6, 0], [6, 2], [0, 2]])]
-    bottom_traction = -5e7
+    bottom_traction = -5e8
     x, y, simplices = mesh.generate_and_import_mesh(contour,
-                                                    max_area=0.001)
+                                                    max_area=0.1)
 
     # Clamp the left side
     mask1 = x == np.amin(x)
@@ -267,18 +267,22 @@ def ex_with_external2():
                                              elem_dict=elem_dict)
     x_new = x + x_displacement
     y_new = y + y_displacement
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(4,3))
     ax.triplot(x, y, simplices, color='r')
     ax.triplot(x_new, y_new, simplices, color='b')
-
-    triangles_before = mesh.all_triangles(simplices, x, y)
-    triangles_after = mesh.all_triangles(simplices, x_new, y_new)
-    area_before = np.sum(fem.calc_areas(triangles_before))
-    area_after = np.sum(fem.calc_areas(triangles_after))
-    print(area_before)
-    print(area_after)
-    np.save('raw_exps2', [x, y, x_new, y_new, simplices])
-    plt.show()
+    ax.set_title(f'Linear deformation of steel. $t=5\cdot 10^8$ N/m')
+    ax.set_aspect('equal')
+    ax.set_xlabel('x, [m]')
+    ax.set_ylabel('y, [m]')
+    fig.tight_layout()
+    # triangles_before = mesh.all_triangles(simplices, x, y)
+    # triangles_after = mesh.all_triangles(simplices, x_new, y_new)
+    # area_before = np.sum(fem.calc_areas(triangles_before))
+    # area_after = np.sum(fem.calc_areas(triangles_after))
+    # print(area_before)
+    # print(area_after)
+    # np.save('raw_exps2', [x, y, x_new, y_new, simplices])
+    fig.savefig('handin/raw.pdf')
 
 
 def ex_load(min_load=0, max_load=-5e7, num=100, max_area=0.01):
@@ -387,7 +391,7 @@ def ex_show_loads():
 
 def ex_squares():
     poissons = [0.1, 0.3, 0.5]
-    max_areas = [1, 0.1, 0.01]
+    max_areas = [0.5, 0.1, 0.01]
     contour = [np.array([[0, 0], [6, 0], [6, 2], [0, 2]])]
     save_file = 'squares'
     poissons2, max_areas2 = np.meshgrid(poissons, max_areas)
@@ -431,3 +435,38 @@ def ex_squares():
                 np.save(save_file, areas)
                 bar.next()
     bar.finish()
+
+
+def ex_analyze_squares():
+
+    poissons = [0.1, 0.3, 0.5]
+    max_areas = [1, 0.1, 0.01]
+    loads = np.linspace(0, 5e7, num=100)
+    poissons2, max_areas2 = np.meshgrid(poissons, max_areas)
+    areas = np.load('squares.npy')
+    start_area = 12
+    res = areas-start_area
+    ratio = res/start_area
+    n = 20
+    
+    fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(9, 9))
+
+    for i in range(poissons2.shape[0]):
+        for j in range(poissons2.shape[1]):
+            exponent = (np.log(ratio[-1, i, j])-np.log(ratio[n, i, j])) / \
+                (np.log(loads[-1])-np.log(loads[n]))
+            print(exponent)
+            axes[i,j].plot(loads, ratio[:, i, j])
+            axes[i,j].scatter(loads[[n, -1]], ratio[[n, -1], i, j])
+            axes[i,j].set_xlabel('Traction [N/m]')
+            axes[i,j].set_ylabel('Relative area increase')
+            axes[i,j].set_yscale('log')
+            axes[i,j].set_xscale('log')
+            axes[i,j].set_title(
+                r'$\nu$ = ' + str(poissons2[i,j]) + ', $A^e$ = ' + str(max_areas2[i,j]) + r', $\Delta A / \Delta t$ = ' + f'{exponent:.3f}')
+    
+    fig.tight_layout()
+    fig.savefig('handin/squares.pdf')
+
+
+ex_analyze_resolution()
