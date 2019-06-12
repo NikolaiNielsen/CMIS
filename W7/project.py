582a357e099bfe7224330901a12d2c4e2a110778
NikolaiNielsen <NikolaiNielsen@outlook.dk>
2019-06-12 18:02:42 +0200

    spaghetti coding my way through this - calc elastic forces

--- W7/project.py
+++ W7/project.py
@@ -54,19 +54,54 @@ def calc_Pe(x, y, simplices, De0Inv, lambda_=1, mu=1):
     De = calc_De(x, y, simplices)
     Fe = De @ De0Inv
 
-    Ee = (Fe.T @ Fe - np.eye(2))/2
-    tr = np.trace(Ee, axis1=1, axis2=2)
+    Ee = np.zeros(Fe.shape)
     I = np.zeros(Ee.shape)
-    for n, i in enumerate(I):
+    for n, _ in enumerate(Ee):
+        Ee[n] = (Fe[n].T @ Fe[n] - np.eye(2))/2
         I[n] = np.eye(2)
+    tr = np.trace(Ee, axis1=1, axis2=2)
     tr2 = np.atleast_3d(tr).reshape((simplices.shape[0],1,1))
     Se = lambda_ * tr2*I + 2*mu*Ee
     Pe = Fe@Se
     return Pe
     
 
+def calc_fe(x, y, simplices, cv, De0Inv, lambda_=1, mu=1):
+    """
+    Calculates the elastic forces for a single vertex.
+    """
+    # We loop over all the parts of the control volume, where code is not 2
+    # (ie, we have an edge which is not on the boundary)
+    pass
+
+def calc_all_fe(x, y, simplices, cvs, De0Inv, lambda_=1, mu=1):
+    N = x.size
+    fe = np.zeros((N,2))
+    Pe = calc_Pe(x, y, simplices, De0Inv, lambda_=1, mu=1)
     
+    for i in range(N):
+        # For each vertex we need the neighbouring simplices:
+        neighbours = mesh.find_neighbouring_simplices(simplices, i)
+        # With the neighbours we need to calculate N and l for these
+        for neigh in neighbours:
+            simp = simplices[neigh]
+            xi = x[i]
+            yi = y[i]
+            non_i = simp[simp != i]
+            xjk = x[non_i]
+            yjk = y[non_i]
+            l = np.sqrt((xjk - xi)**2 + (yjk - yi)**2)/2
+            Ne = np.zeros((2,2))
+            Ne[:,0] = xjk-xi
+            Ne[:,1] = yjk-yi
+            P = Pe[neigh]
+            fi = -0.5*P@(Ne[0]*l[0] + Ne[1]*l[1])
+            fe[i] += fi
+    return fe
+
+    
+
 
 x, y, simplices, cvs = fvm.load_cvs_mat('control_volumes.mat')
 De0inv = calc_De0Inv(x, y, simplices)
-calc_Pe(x, y, simplices, De0inv)
+fe = calc_all_fe(x, y, simplices, cvs, De0inv)
